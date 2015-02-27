bd0183dde6804547cc2673b3f9a8c50abb8e4361
Seif Lotfy <seif@lotfy.com>
2015-02-27 19:01:15 +0100

    Cleanup testHash.js
    Get rid of duplicate json and add extra assertion

--- src/tests/testHash.js
+++ src/tests/testHash.js
@@ -17,25 +17,11 @@ var data1 = {
   payload: []
 };
 
+
 var data2 = {
   type: 'click',
   actor: 'user1',
   subjects: [{
-    id: 'http://yahoo.com',
-    type: 'website',
-    text: 'Yahoo!',
-    domain: 'yahoo.com'
-  }],
-  timestamp: 0,
-  typeDomain: 'clickFromWebsite',
-  origin: 'http://yahoo.com',
-  payload: []
-};
-
-var data3 = {
-  type: 'click',
-  actor: 'user1',
-  subjects: [{
     id: 'http://google.com',
     type: 'website',
     text: 'Google Search',
@@ -47,24 +33,15 @@ var data3 = {
   payload: []
 };
 
-var data3 = {
-  type: 'click',
-  actor: 'user1',
-  subjects: [{
-    id: 'http://google.com',
-    type: 'website',
-    text: 'Google Search',
-    domain: 'google.com',
-  }],
-  timestamp: Date.now(),
-  typeDomain: 'clickFromWebsite',
-  origin: 'http://youtube.com',
-  payload: []
-};
 
 var event1 = event.createEventFromData(data1);
-var event2 = event.createEventFromData(data2);
-var event3 = event.createEventFromData(data3);
+var event2 = event.createEventFromData(data1);
+var event3 = event.createEventFromData(data2);
+
+data1.subjects[0].id = 'http://facebook.com';
+
+var event4 = event.createEventFromData(data1);
 
 assert.ok(event1.id === event2.id); // identical hash codes
 assert.ok(event1.id !== event3.id); // different hash codes
+assert.ok(event1.id !== event4.id); // different hash codes
